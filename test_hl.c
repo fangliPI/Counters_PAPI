e965c4c4bbaa076bf59d526b1079a1aa331095fd
Fangli Pi <hpcfapix@akuma.hlrs.de>
2015-08-12 14:13:49 +0200

    updated for my test

--- test_hl.c
+++ test_hl.c
@@ -22,8 +22,8 @@ static void test_fail(char *file, int line, char *call, int retval);
 int main(int argc, char **argv) {
   extern void dummy(void *);
   float matrixa[INDEX][INDEX], matrixb[INDEX][INDEX], mresult[INDEX][INDEX];
-  float real_time, proc_time, mflops;
-  long long flpins;
+  float real_time_begin, real_time_end, proc_time_begin, proc_time_end, mflips_begin, mflips_end, mflops_begin, mflops_end, real_time, proc_time, mflips, mflops;
+  long long flpins_begin, flpins_end, flpins, flpops_begin, flpops_end, flpops;
   int retval;
   int i,j,k;
 
@@ -33,7 +33,10 @@ int main(int argc, char **argv) {
     matrixa[0][i] = matrixb[0][i] = rand()*(float)1.1; }
 
   /* Setup PAPI library and begin collecting data from the counters */
-  if((retval=PAPI_flops( &real_time, &proc_time, &flpins, &mflops))<PAPI_OK)
+  if((retval=PAPI_flips( &real_time_begin, &proc_time_begin, &flpins_begin, &mflips_begin))<PAPI_OK)
+    test_fail(__FILE__, __LINE__, "PAPI_flips", retval);
+
+  if((retval=PAPI_flops( &real_time_begin, &proc_time_begin, &flpops_begin, &mflops_begin))<PAPI_OK)
     test_fail(__FILE__, __LINE__, "PAPI_flops", retval);
 
   /* Matrix-Matrix multiply */
@@ -43,11 +46,20 @@ int main(int argc, char **argv) {
       mresult[i][j]=mresult[i][j] + matrixa[i][k]*matrixb[k][j];
 
   /* Collect the data into the variables passed in */
-  if((retval=PAPI_flops( &real_time, &proc_time, &flpins, &mflops))<PAPI_OK)
+  if((retval=PAPI_flips( &real_time_end, &proc_time_end, &flpins_end, &mflips_end))<PAPI_OK)
+    test_fail(__FILE__, __LINE__, "PAPI_flips", retval);
+  if((retval=PAPI_flops( &real_time_end, &proc_time_end, &flpops_end, &mflops_end))<PAPI_OK)
     test_fail(__FILE__, __LINE__, "PAPI_flops", retval);
 
-  printf("Real_time:\t%f\nProc_time:\t%f\nTotal flpins:\t%lld\nMFLOPS:\t\t%f\n",
-  real_time, proc_time, flpins, mflops);
+  real_time = real_time_end - real_time_begin;
+  proc_time = proc_time_end - proc_time_begin;
+  flpins = flpins_end - flpins_begin; 
+  flpops = flpops_end - flpops_begin;
+  mflips = mflips_end - mflips_begin; 
+  mflops = mflops_end - mflops_begin;
+
+  printf("Real_time:\t%f\nProc_time:\t%f\nTotal flpins:\t%lld\nTotal flpops:\t%lld\nMFLIPS:\t\t%f\nMFLOPS:\t\t%f\n",
+  real_time, proc_time, flpins, flpops, mflips, mflops);
   printf("%s\tPASSED\n", __FILE__);
   PAPI_shutdown();
   exit(0);
